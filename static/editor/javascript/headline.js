9d199596729f46c2978f21c267c0772e9bf734f7
Richard Wandel <ricwa059@student.liu.se>
2014-07-15 10:20:19 +0200

    Added missing files

---
+++ static/editor/javascript/headline.js
@@ -0,0 +1,17 @@
+$('document').ready(function(){
+    var url = document.URL;
+    var text;
+    if (url.indexOf("general") >= 0) {
+	text = "1. Bestäm namn och startmedia";
+    }
+    else if (url.indexOf("stations") >= 0) {
+	text = "2. Lägg till stationer på kartan";
+    }
+    else if (url.indexOf("media") >= 0) {
+	text = "3. Lägg till media";
+    }
+    else if (url.indexOf("publish") >= 0) {
+	text = "4. Publicera, skriv ut och färdigställ";
+    }
+    document.getElementById("headlineName").innerHTML = text;
+});
